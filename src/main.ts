be4d8935478d76859f525d51a700778fba2a4f34
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-12 01:11:11 +0000

    chore: add swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { limiter } from './config/policies/rate-limit.config';
 import { corsConfig } from './config/policies/cors.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Example')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('examples') // Adicione tags conforme necessário
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Acesse a documentação em /api
 
   //proteção contra ataque XSS e outros
   app.use(helmet());
@@ -16,6 +27,6 @@ async function bootstrap() {
   //proteção contra origens desconhecidas;
   app.enableCors(corsConfig);
 
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();

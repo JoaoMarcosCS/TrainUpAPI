9934157aff0a37e2b561b289af031e9db25f41a0
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-12 01:05:26 +0000

    chore: add helmet security

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { limiter } from './config/policies/rate-limit.config';
+import { corsConfig } from './config/policies/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //proteção contra ataque XSS e outros
+  app.use(helmet());
+
+  //proteção contra DDOS
+  app.use(limiter);
+
+  //proteção contra origens desconhecidas;
+  app.enableCors(corsConfig);
+
   await app.listen(3001);
 }
 bootstrap();

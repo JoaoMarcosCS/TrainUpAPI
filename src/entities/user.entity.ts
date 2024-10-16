8dc035d71593c48f5ee39b8a96fad0cab62571e7
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-16 21:30:36 +0000

    chore: add props from google oauth

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity } from "typeorm";
+import { BasicEntity } from "./base-entity";
+import { Gender } from "src/enums/gender.enum";
+
+@Entity('user')
+export class User extends BasicEntity {
+
+    @Column({ type: "text" })
+    name: string;
+
+    @Column({ type: "text", unique: true })
+    googleId: string;
+
+    @Column({ type: "text", unique: true })
+    email: string;
+
+    @Column({ type: "text" })
+    avatarUrl: string;
+
+    @Column({ type: "text" })
+    password: string;
+
+    @Column({ type: "decimal" })
+    height: number
+
+    @Column({ type: "decimal" })
+    weight: number;
+
+    @Column({ type: "enum", enum: Gender })
+    gender: Gender;
+
+    @Column({ type: "date" })
+    birthday: Date;
+
+    @Column({ type: "text", nullable: true, default: "Nada escrito ainda :(" })
+    about?: string;
+}

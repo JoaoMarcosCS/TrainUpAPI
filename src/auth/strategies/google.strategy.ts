17aa66e14d0ecf7e98ba9c547e648d4de21f5e0b
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-18 17:03:38 +0000

    fix: return of data from oauth login

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -1,9 +1,7 @@
-import { v4 as uuidv4 } from 'uuid';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { AuthService } from '../auth.service';
 import { EnvironmentService } from '../../environment/environment.service';
-import { ProfileGoogle } from '../types/user-profile.type';
 import { Inject } from '@nestjs/common';
 
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
@@ -11,27 +9,27 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     constructor(private readonly authService: AuthService,
         @Inject(EnvironmentService) private readonly environment: EnvironmentService,
     ) {
- 
         super({
             clientID: environment.GOOGLE_CLIENT_ID,
             clientSecret: environment.GOOGLE_CLIENT_SECRET,
-            callbackURL: environment.URL_CALLBACK_LOCAL,
+            callbackURL: environment.URL_CALLBACK_CODESPACE,
             scope: ['email', 'profile']
         })
     }
 
-    async validator(profile: ProfileGoogle, done: VerifyCallback) {
+    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any>{
 
-        const { googleId, displayName, emails, photos }: ProfileGoogle = profile;
-
+        const { googleId, displayName, emails, photos } = profile;
+        console.log("\n Profile google received\n " + JSON.stringify(profile) + "\n End of profile received\n\n");
         const user: any = {
             googleId: googleId,
             email: emails[0].value,
             name: displayName,
             avatarUrl: photos[0].value,
+            accessToken,
+
         };
 
-        // const validatedUser = await this.authService.verifyOrCreateUser(user);
         done(null, user);
 
     }

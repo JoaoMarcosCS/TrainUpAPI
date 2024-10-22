c6a1b60b4d04c718c1ab2778f4f5e428c7aa255d
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-22 23:38:04 +0000

    refact: changed the way to make a request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { FindUserByEmailQuery } from 'src/user/queries/find-user-by-email/find-u
 import { comparePassword } from 'src/utils/compare-password.utils';
 import { FindUserByEmailDto } from 'src/user/queries/find-user-by-email/find-user-by-email.dto';
 import { ERROR_MESSAGES } from 'src/constants/error-message.constants';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -20,35 +21,38 @@ export class AuthService {
         private readonly dataSource: DataSource,
         private readonly queryBus: QueryBus
     ) { }
-    
-    async signInWithGoogle({email, id}) {
-        
-        const accessToken = await this.jwtService.sign({email, id});
+
+    async signInWithGoogle({ email, id }) {
+
+        const accessToken = await this.jwtService.sign({ email, id });
 
         return {
             accessToken,
         }
     }
 
-    async signInWithPassword({ email, password}){
+    async signInWithPassword({ email, password }) {
 
-        const query = plainToClass(FindUserByEmailQuery, email);
-        const user: FindUserByEmailDto = await this.queryBus.execute(query);
-
-        if(!user) throw new NotFoundException(ERROR_MESSAGES.USER_NOT_FOUND);
+        const user = await this.dataSource.manager.findOne(User, {
+            where:{
+                email: email
+            }
+        })
+        
+        if (!user) throw new NotFoundException(ERROR_MESSAGES.USER_NOT_FOUND);
 
         const isCorrectPassword = await comparePassword(password, user.password);
 
-        if(!isCorrectPassword) throw new BadRequestException(ERROR_MESSAGES.INCORRECT_PASSWORD)
+        if (!isCorrectPassword) throw new BadRequestException(ERROR_MESSAGES.INCORRECT_PASSWORD)
 
         const payload = {
             email: user.email,
             id: user.id,
         }
 
-        const token = await this.jwtService.sign({...payload, type: "accesss"});
+        const token = await this.jwtService.sign({ ...payload, type: "accesss" });
 
     }
 
-    
+
 }

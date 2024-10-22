ba4a3b630c15890f4b4ee049f6ad408f1a6c0c64
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-22 23:11:28 +0000

    feat: add global validation

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,18 @@ import { limiter } from './config/policies/rate-limit.config';
 import { corsConfig } from './config/policies/cors.config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swagger/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('doc', app, document); // Acesse a documentação em /doc
 
   //proteção contra ataque XSS e outros
   app.use(helmet());
-
-  //config para usar o oatuh
-  // app.use({'trust proxy': 1}); 
-
-  //proteção contra DDOS
-  // app.use(limiter);
 
   //proteção contra origens desconhecidas;
   app.enableCors(corsConfig);

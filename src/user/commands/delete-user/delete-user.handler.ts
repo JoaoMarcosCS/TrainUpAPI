e051d5b627adf9537803ec7681d04eb4c4eebeb5
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-22 00:47:31 +0000

    feat: add cqrs to delete user

---
+++ src/user/commands/delete-user/delete-user.handler.ts
@@ -0,0 +1,32 @@
+import { ICommandHandler } from "@nestjs/cqrs";
+import { DeleteUserCommand } from "./delete-user.command";
+import { InjectDataSource } from "@nestjs/typeorm";
+import { DataSource } from "typeorm";
+import { User } from "src/entities/user.entity";
+
+export class DeleteUserHandler implements ICommandHandler<DeleteUserCommand, boolean | null>{
+    
+    constructor(
+        @InjectDataSource()
+        private readonly dataSource: DataSource,
+    ){}
+    
+    async execute(command: DeleteUserCommand): Promise<boolean> {
+
+        return await this.dataSource.transaction(async (db) => {
+            const user = await db.findOne(User, {
+                where: {
+                    id: command.id
+                }
+            })
+
+            if(!user) return false;
+
+            db.delete(User, user);
+
+            return true;
+
+        })
+    }
+
+}

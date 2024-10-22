131ed97eadfa8f4b2dd85da2596c4cc100f88d6a
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-22 23:45:18 +0000

    feat: add message erro to cannot create user

--- src/constants/error-message.constants.ts
+++ src/constants/error-message.constants.ts
@@ -1,4 +1,5 @@
 export const ERROR_MESSAGES = {
     USER_NOT_FOUND: "Usuário não encontrado",
     INCORRECT_PASSWORD: "Senha inválida",
+    CANNOT_CREATE_USER: "Não foi possível cria o usuário",
 }

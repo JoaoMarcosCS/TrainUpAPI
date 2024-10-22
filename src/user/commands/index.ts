54bd8fcd760b8e525da36dddfbb47125a1c905ff
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-22 23:39:30 +0000

    refact: add the handlers

--- src/user/commands/index.ts
+++ src/user/commands/index.ts
@@ -1,3 +1,5 @@
-import { CreateUserCommand } from "./create-user/create-user.command";
+import { CreateUserHandler } from "./create-user/create-user.handler";
+import { DeleteUserHandler } from "./delete-user/delete-user.handler";
+import { UpdateUserHandler } from "./udpate-user/update-user.handler";
 
-export const CommandHandlers = [CreateUserCommand];
+export const CommandHandlers = [CreateUserHandler, DeleteUserHandler, UpdateUserHandler];

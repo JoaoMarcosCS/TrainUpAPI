167ea7b8537c17204ce0d330ff09cf6918528479
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-23 00:47:31 +0000

    refact: removed unesud parameters

--- src/auth/guards/jwt/jwt.guard.ts
+++ src/auth/guards/jwt/jwt.guard.ts
@@ -7,22 +7,8 @@ import { IS_PUBLIC_KEY } from 'src/auth/decorators/public-route.decorator';
 export class JwtGuard extends AuthGuard('jwt') {
 
     constructor(
-        private reflector: Reflector,
     ) {
         super();
     }
 
-    // //validação para verifica se a tem o decorator da rota pública
-    // canActivate(context: ExecutionContext) {
-
-    //     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-    //         context.getClass(),
-    //         context.getHandler()
-    //     ]);
-
-    //     if (!isPublic) return false;
-
-    //     return super.canActivate(context)
-
-    // }
 }

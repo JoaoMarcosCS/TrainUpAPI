84d131fc6aa5ec46c706dd88b2839374b8515744
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-23 00:39:49 +0000

    feat: add origin from github codespace

--- src/config/policies/cors-allowed-origins.ts
+++ src/config/policies/cors-allowed-origins.ts
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
     'http://localhost:3001',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    "https://reimagined-barnacle-gwxpxv5r44p39w4v-3001.app.github.dev", //codespace
   ];
   

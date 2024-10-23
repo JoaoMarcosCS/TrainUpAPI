5dc1ec81bc418c7c76eae55846f2b8a958964b6f
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-23 00:45:30 +0000

    doc: add a title to doc

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -1,8 +1,8 @@
 import { DocumentBuilder } from "@nestjs/swagger";
 
 export const swaggerConfig = new DocumentBuilder()
-    .setTitle('API Example')
-    .setDescription('API description')
+    .setTitle('TrainUp API Doc')
+    .setDescription('Essa é a documentação da API do projeto TrainUp feita')
     .setVersion('1.0')
     .addTag('examples') // Adicione tags conforme necessário
     .build();

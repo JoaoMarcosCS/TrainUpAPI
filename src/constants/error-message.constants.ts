d1652499a26705b100eb012464bd730ca17ab744
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-23 00:35:24 +0000

    feat: add a bad request message

--- src/constants/error-message.constants.ts
+++ src/constants/error-message.constants.ts
@@ -2,4 +2,5 @@ export const ERROR_MESSAGES = {
     USER_NOT_FOUND: "Usuário não encontrado",
     INCORRECT_PASSWORD: "Senha inválida",
     CANNOT_CREATE_USER: "Não foi possível cria o usuário",
+    BAD_REQUEST: "Os dados passados não estão corretos ou não existe na base de dados",
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Get, NotFoundException, Param, Post, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, ParseUUIDPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { plainToClass } from 'class-transformer';
 import { FindUserByEmailQuery } from './queries/find-user-by-email/find-user-by-email.query';
 import { JwtGuard } from 'src/auth/guards/jwt/jwt.guard';
 import { CreateUserCommand } from './commands/create-user/create-user.command';
 import { CreateUserDto } from './commands/create-user/create-user.dto';
+import { ERROR_MESSAGES } from 'src/constants/error-message.constants';
+import { UpdateUserDto } from './commands/udpate-user/update-user.dto';
+import { UpdateUserCommand } from './commands/udpate-user/update-user.command';
+import { DeleteUserCommand } from './commands/delete-user/delete-user.command';
 
 @Controller('user')
 export class UserController {
@@ -18,13 +22,15 @@ export class UserController {
     @Post()
     async cretae(
         @Body()
-        dto: CreateUserDto// já válida o dto antes de chegar na função
+        dto: CreateUserDto
     ) {
         const command = plainToClass(CreateUserCommand, dto);
 
         const id = await this.commandBus.execute(command);
 
-        if (!id) throw new NotFoundException("User ")
+        if (!id) throw new NotFoundException(ERROR_MESSAGES.CANNOT_CREATE_USER);
+
+        return id;
     }
 
     @UseGuards(JwtGuard)
@@ -35,7 +41,6 @@ export class UserController {
     ) {
         const query = plainToClass(FindUserByEmailQuery, { email });
 
-        //aqui o queryBus irá encontrar um handler associado a essa query
         const user = await this.queryBus.execute(query);
 
         if (!user) throw new NotFoundException("User not found");
@@ -43,4 +48,40 @@ export class UserController {
         return user;
     }
 
+    @UseGuards(JwtGuard)
+    @Patch(":id")
+    async update(
+        @Param('id', ParseUUIDPipe)
+        id: string,
+        @Body()
+        dto: UpdateUserDto
+    ) {
+
+        const command = plainToClass(UpdateUserCommand, {
+            ...dto,
+            id
+        });
+
+        const result = await this.commandBus.execute(command);
+
+        if (!result) throw new BadRequestException(ERROR_MESSAGES.BAD_REQUEST);
+
+        return result
+    }
+
+    @UseGuards(JwtGuard)
+    @Delete(":id")
+    async delete(
+        @Param('id', ParseUUIDPipe)
+        id: string
+    ) {
+
+        const command = plainToClass(DeleteUserCommand, { id });
+
+        const result = await this.commandBus.execute(command);
+
+        if (!result) throw new BadRequestException(ERROR_MESSAGES.BAD_REQUEST);
+
+        return result
+    }
 }

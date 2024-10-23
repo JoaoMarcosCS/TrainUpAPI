f07b9a524819b93d4cda78f1cec9c3d326eb9775
João Marcos <jmcsjoaomarcos@gmail.com>
2024-10-23 00:42:55 +0000

    refact: sintaxe

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,37 +5,37 @@ import { EnvironmentService } from 'src/environment/environment.service';
 import { DataSource } from 'typeorm';
 
 @Module({
-    imports:[
-        TypeOrmModule.forRootAsync({
-            imports:[EnvironmentModule],
-            inject:[EnvironmentService],
-            useFactory:(env: EnvironmentService) => ({
-                type: "postgres",
-                logging: true,
-                entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
-                url: env.DATABASE_CONNECTION_STRING,
-                synchronize: true
-            }),
-        }),
-    ],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [EnvironmentModule],
+      inject: [EnvironmentService],
+      useFactory: (env: EnvironmentService) => ({
+        type: "postgres",
+        logging: true,
+        entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
+        url: env.DATABASE_CONNECTION_STRING,
+        synchronize: true
+      }),
+    }),
+  ],
 
 })
 
 export class DatabaseModule implements OnModuleInit {
-    private readonly logger = new Logger(DatabaseModule.name);
-  
-    constructor(@InjectDataSource() private dataSource: DataSource) {}
-  
-    async onModuleInit() {
-      try {
-        // Verifica se a conexão está estabelecida corretamente
-        this.logger.log('Database connection established successfully!');
-  
-        // Loga as entidades carregadas
-        const entities = this.dataSource.entityMetadatas.map((entity) => entity.name);
-        this.logger.log(`Loaded entities: ${entities.join(', ')}`);
-      } catch (error) {
-        this.logger.error('Error during database initialization', error);
-      }
+  private readonly logger = new Logger(DatabaseModule.name);
+
+  constructor(@InjectDataSource() private dataSource: DataSource) { }
+
+  async onModuleInit() {
+    try {
+      // Verifica se a conexão está estabelecida corretamente
+      this.logger.log('Database connection established successfully!');
+
+      // Loga as entidades carregadas
+      const entities = this.dataSource.entityMetadatas.map((entity) => entity.name);
+      this.logger.log(`Loaded entities: ${entities.join(', ')}`);
+    } catch (error) {
+      this.logger.error('Error during database initialization', error);
     }
   }
+}
